812979244b46edaa5566ea4be537957e15583093
freecates <ramongil@ramongil.cat>
2016-11-30 12:33:34 +0100

    revsiat de nou el build.sh

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -14,17 +14,17 @@ npm install
 gulp build
 
 # Checkout master and remove everything
-git clone https://${GH_TOKEN}@github.com/quidcec/quidcec.github.io.git ../quidcec.github.io.git.master
-cd ../quidcec.github.io.git.master
+git clone https://${GH_TOKEN}@github.com/quidcec/quidcec.github.io.git ../quidcec.github.io.master
+cd ../quidcec.github.io.master
 git checkout master
 rm -rf *
 
 # Copy generated HTML site from source branch in original repo.
 # Now the master branch will contain only the contents of the _site directory.
-cp -R ../quidcec.github.io.git/_site/* .
+cp -R ../quidcec.github.io/_site/* .
 
 # Make sure we have the updated .travis.yml file so tests won't run on master.
-cp ../quidcec.github.io.git/.travis.yml .
+cp ../quidcec.github.io/.travis.yml .
 git config user.email ${GH_EMAIL}
 git config user.name "bot-quid"
 
