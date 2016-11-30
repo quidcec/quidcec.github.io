c0264d2c6498b4979a6c86dbeaf02ef9a31f8987
Ramon <ramongil@ramongil.cat>
2016-11-30 17:04:09 +0100

    Delete one-signal-init.js

--- one-signal-init.js
+++
@@ -1,8 +0,0 @@
-var OneSignal = window.OneSignal || [];
-    OneSignal.push(["init", {
-      appId: "85adb3aa-ffa1-42a5-bad3-22dac283cca3",
-      autoRegister: false,
-      notifyButton: {
-        enable: true
-      }
-    }]);
