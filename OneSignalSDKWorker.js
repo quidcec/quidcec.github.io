255eabaf30c0a92e8eb1776f2ee8c3b277798899
freecates <ramongil@ramongil.cat>
2016-11-30 14:30:07 +0100

    afegit l'script d'inicialització one signal via import.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,10 @@
 importScripts('/sw.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
+var OneSignal = window.OneSignal || [];
+    OneSignal.push(["init", {
+      appId: "85adb3aa-ffa1-42a5-bad3-22dac283cca3",
+      autoRegister: false,
+      notifyButton: {
+        enable: true
+      }
+    }]);
