e7988bff9b94e0e67bbbe716b9a2e22bc21df5e3
freecates <ramongil@ramongil.cat>
2016-11-30 11:18:36 +0100

    modificat l'script build.

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -11,7 +11,7 @@ bundle check || bundle install
 npm install
 
 # Build the site.
-gulp
+gulp build
 
 # Checkout master and remove everything
 git clone https://${GH_TOKEN}@github.com/quidcec/quidcec.github.io.git ../quidcec.github.io.git.master
