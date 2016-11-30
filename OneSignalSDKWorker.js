313012878c236977aeb58b60f50644b399ad604e
freecates <ramongil@ramongil.cat>
2016-11-30 18:15:33 +0100

    service worker one-signal-init via import.

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,10 +1,3 @@
 importScripts('/sw.js');
+importScripts('/one-signal-init.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDK.js');
-var OneSignal = window.OneSignal || [];
-    OneSignal.push(["init", {
-      appId: "85adb3aa-ffa1-42a5-bad3-22dac283cca3",
-      autoRegister: false,
-      notifyButton: {
-        enable: true
-      }
-    }]);
---
+++ one-signal-init.js
@@ -0,0 +1,8 @@
+var OneSignal = OneSignal || [];
+OneSignal.push(["init", {
+appId: "85adb3aa-ffa1-42a5-bad3-22dac283cca3",
+autoRegister: false,
+notifyButton: {
+    enable: true
+}
+}]);
